8d665dd1e36f86584a8d1bbd3f30588b4d9b87e2
Shegare <228shadowfox228@gmail.com>
2026-09-16 22:04:17 +0300

    Revert "Фикс КПК (#74)"
    This reverts commit 1ba282722d5da000f2a774053f06d995ea76e55b.

--- Content.Client/CartridgeLoader/Cartridges/CrewManifestUi.cs
+++ Content.Client/CartridgeLoader/Cartridges/CrewManifestUi.cs
@@ -15,8 +15,7 @@ public sealed partial class CrewManifestUi : UIFragment
 
     public override void Setup(BoundUserInterface userInterface, EntityUid? fragmentOwner)
     {
-        if (_fragment is null || _fragment.Disposed) // SIS-Cartridge_FIX | TODO: Кинуть офффам
-            _fragment = new CrewManifestUiFragment();
+        _fragment = new CrewManifestUiFragment();
     }
 
     public override void UpdateState(BoundUserInterfaceState state)
